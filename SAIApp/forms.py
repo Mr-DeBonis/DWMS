10afc8ddfcdde46b4b4b619c0b5e4bc95dd358f8
Mr-DeBonis <ignacio.debonis7@gmail.com>
2025-09-15 01:54:40 -0300

    Añade campo de otro transporte al ingreso de despachos

--- SAIApp/forms.py
+++ SAIApp/forms.py
@@ -22,7 +22,6 @@ class FormGuiaHeader(forms.ModelForm):
         }
 
 
-
 class FormDespacho(forms.ModelForm):
     fecha_despacho = forms.DateTimeField(
         input_formats= ['%Y-%m-%dT%H:%M'],
@@ -34,13 +33,16 @@ class FormDespacho(forms.ModelForm):
 
     class Meta:
         model = dwms_despacho
-        fields = ['transporte', 'fecha_despacho', 'nota']
+        fields = ['transporte', 'fecha_despacho', 'nota', 'otro_transporte']
 
         widgets = {
             'transporte': forms.Select(attrs={
                 'class': 'form-control',
                 'queryset': dwms_transporte.objects.filter(activo=True),
                 'empty_label': None,
+            }),
+            'otro_transporte': forms.TextInput(attrs={
+                'class': 'form-control',
             }),
             'nota': forms.Textarea(attrs={
                 'rows': 3,
@@ -51,9 +53,22 @@ class FormDespacho(forms.ModelForm):
             })
         }
 
-
     def __init__(self, *args, **kwargs):
         super(FormDespacho, self).__init__(*args, **kwargs)
         self.fields['transporte'].queryset = dwms_transporte.objects.filter(activo=True)
         self.fields['transporte'].empty_label = None
 
+    def clean(self):
+        cleaned_data = super(FormDespacho, self).clean()
+        transporte = cleaned_data.get('transporte')
+        otro_transporte = cleaned_data.get('otro_transporte')
+
+        # Check if transporte is OTRO
+        if transporte and transporte.nombre.strip().upper() == 'OTRO':
+            if not otro_transporte:
+                self.add_error('otro_transporte', 'Este campo es requerido cuando el transporte es OTRO.')
+        else:
+            # Clear the field if it's not OTRO
+            cleaned_data['otro_transporte'] = ''
+
+        return cleaned_data
